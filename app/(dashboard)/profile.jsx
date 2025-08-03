6b7b2fb2b7ea654eb8bc467cfda108a071146802
zhouming <minerchow@126.com>
2025-08-03 16:44:38 +0800

    update

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -1,27 +1,10 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet,Text } from 'react-native'
 
 import Spacer from "../../components/Spacer"
 import ThemedText from "../../components/ThemedText"
 import ThemedView from "../../components/ThemedView"
-
-const Profile = () => {
-  return (
-    <ThemedView style={styles.container}>
-
-      <ThemedText title={true} style={styles.heading}>
-        Your Email
-      </ThemedText>
-      <Spacer />
-
-      <ThemedText>Time to start reading some books...</ThemedText>
-      <Spacer />
-
-    </ThemedView>
-  )
-}
-
-export default Profile
-
+import ThemedButton from '../../components/ThemedButton'
+import { useUser } from '../../hooks/useUser';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -34,3 +17,24 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 })
+const Profile = () => {
+  const { logout } = useUser()
+  return (
+    <ThemedView style={styles.container}>
+
+      <ThemedText title={true} style={styles.heading}>
+        Your Email
+      </ThemedText>
+      <Spacer />
+
+      <ThemedText>Time to start reading some books...</ThemedText>
+      <Spacer />
+      <ThemedButton  style={styles.button}>
+        <Text style={{ color: '#f2f2f2' }}>Logout</Text>
+      </ThemedButton>
+    </ThemedView>
+  )
+}
+
+export default Profile
+
--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -27,7 +27,8 @@ export function UserProvider({ children }) {
   } 
 
   async function logout() {
-
+    await account.deleteSession("current")
+    setUser(null)
   }
 
   return (
